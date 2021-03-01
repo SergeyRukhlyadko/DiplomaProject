e1619c7cfa9d5ffcb982ca31f8f6387374b7846d
Sergey-PC <cer1990@gmail.com>
2021-03-01 14:44:17 +0300

    code cleanup

--- src/main/java/org/diploma/app/controller/DefaultController.java
+++ src/main/java/org/diploma/app/controller/DefaultController.java
@@ -1,22 +1,12 @@
 package org.diploma.app.controller;
 
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
-import org.diploma.app.service.GeneralService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Controller
 class DefaultController {
-
-    GeneralService generalService;
-
-    public DefaultController(GeneralService generalService) {
-        this.generalService = generalService;
-    }
 
     @GetMapping("/")
     String index() {
@@ -24,7 +14,7 @@ class DefaultController {
     }
 
     @RequestMapping(method = {RequestMethod.OPTIONS, RequestMethod.GET}, value = "/**/{path:[^\\\\.]*}")
-    public String redirectToIndex() {
+    public String forwardToIndex() {
         return "forward:/";
     }
 }

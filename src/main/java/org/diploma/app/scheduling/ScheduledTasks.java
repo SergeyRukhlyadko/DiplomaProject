a104ec4c9ff173ccf648532b7216de075f1045d8
Sergey-PC <cer1990@gmail.com>
2021-03-19 14:16:00 +0300

    added transaction on a scheduled task

--- src/main/java/org/diploma/app/scheduling/ScheduledTasks.java
+++ src/main/java/org/diploma/app/scheduling/ScheduledTasks.java
@@ -3,6 +3,7 @@ package org.diploma.app.scheduling;
 import org.diploma.app.repository.CaptchaCodesRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -19,6 +20,7 @@ public class ScheduledTasks {
         All captcha created over one hour are deleted
      */
     @Scheduled(cron = "@hourly")
+    @Transactional
     public void clearCaptcha() {
         captchaCodesRepository.deleteByTimeLessThen(LocalDateTime.now().minusHours(1));
     }
